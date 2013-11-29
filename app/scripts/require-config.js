74c3c7aab6ee183efe7b1f5fad61f814517db404
Jeff Smith <jeff.r.smith@gmail.com>
2013-11-28 17:39:37 -0800

    Upgrade to latest versions of js libraries

--- app/scripts/require-config.js
+++ app/scripts/require-config.js
@@ -5,13 +5,11 @@
  */
 var require = {
 
-//    baseUrl: 'scripts',
-
     paths: {
-        jquery: 'vendor/jquery-1.9.1-min',
-        underscore: 'vendor/underscore-1.4.4-min',
-        backbone: 'vendor/backbone-1.0.0-min',
-        text: 'vendor/text-2.0.7-min'
+        jquery: 'vendor/jquery-2.0.3-min',
+        underscore: 'vendor/underscore-1.5.2-min',
+        backbone: 'vendor/backbone-1.1.0-min',
+        text: 'vendor/text-2.0.10'
     },
     map: {
         '*': {
